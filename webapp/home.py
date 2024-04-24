36962eba008e87ebd8de16cc24b96e756e769f54
Sophie Arana <arana.sophie@gmail.com>
2024-04-24 10:16:18 +0100

    adding logos & survey landing page text #8

--- webapp/home.py
+++ webapp/home.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from config import USER_ID_STATE_KEY
 import uuid
+from PIL import Image
+
 
 if USER_ID_STATE_KEY not in st.session_state:
     st.session_state[USER_ID_STATE_KEY] = str(uuid.uuid4())
@@ -20,24 +22,30 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# st.sidebar.markdown("# TEA-DT Survey")
+# Logo and Navigation
+col1, col2, col3 = st.columns((3, 2, 3))
+with col1:
+    st.image(Image.open("webapp/img/Alan_Turing_Institute_logo.png"))
+with col3:
+    st.image(Image.open("webapp/img/DTHub_logo.jpg"))
 
-st.markdown("# Welcome to our TEA-DT Survey")
+st.write("#")
+
+# Title
+st.markdown("# Welcome to the Community Pulse Check on Assurance of Digital Twin Systems")
+
+# Description
 st.markdown(
-    "This is a [streamlit](https://streamlit.io) app, which can be easily "
-    "edited to gather and present data. It is written in Python and compiled "
-    "into HTML, "
-    "CSS, and Javascript. As such, it can also support backend data analysis "
-    "(e.g. "
-    "quantitative and qualititative analysis of the survey data."
-)
-
-st.caption(
-    "Read more about the options on the streamlit documentation site:"
-    "[https://docs.streamlit.io/](https://docs.streamlit.io/)"
+    "Are you confident that digital twins are trustworthy and ethical? Share your approaches and challenges in ensuring and promoting trustworthiness across the digital twin ecosystem, and help shape the future assurance standards. "
 )
 
 if st.button("Next"):
     # Redirect to the next section of the survey
     st.write("Redirecting to the survey...")
     switch_page("Community")
+
+
+# Funding
+st.caption(
+    "This work is conducted as a part of the Trustworthy and Ethical Assurance of Digital Twins project, and is generously funded by an award from the UKRI’s Arts and Humanities Research Council as part of the BRAID programme. You can read more about the project here: https://www.turing.ac.uk/research/research-projects/trustworthy-and-ethical-assurance-digital-twins-tea-dt "
+)
